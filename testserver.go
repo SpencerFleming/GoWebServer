591c4b05ee08c08807aca9defc20e957fb45dc43
Spencer Fleming <spencerfleming@u.boisestate.edu>
2019-12-01 19:44:15 -0700

    Corrected server address

--- testserver.go
+++ testserver.go
@@ -95,7 +95,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1" + port,
+		Addr:         port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
